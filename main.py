06ddf25116333fc6412905fd410a7b1d625589e6
Hristo Shirov <hristo.shirov@mentormate.com>
2024-09-16 22:17:06 +0300

    Add ruff (linter)

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from db import books
 
 app = FastAPI()
 
-@app.get('/books')
+
+@app.get("/books")
 async def get_books(offset: int = 0, limit: int = 10):
     return books[offset : offset + limit]
 
-@app.get('/books/{id}')
+
+@app.get("/books/{id}")
 async def get_book(id: int):
     for book in books:
-        if book['id'] == id:
+        if book["id"] == id:
             return book
     raise HTTPException(status_code=404, detail="Book not found")
